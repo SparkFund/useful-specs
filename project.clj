8e443455e8b77731d4c9140c8c0577d4fced79c3
Donald Ball <donald.ball@sparkfund.co>
2016-07-07 14:27:04 -0400

    Initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject sparkfund/useful-specs "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha9"]
+                 [org.clojure/test.check "0.9.0"]])
