62bc33a70a74cf21cf8c21abba9c5c245f90d39f
Donald Ball <donald.ball@sparkfund.co>
2016-07-07 17:02:04 -0400

    Add ip address and email address specs

--- src/specs/internet.clj
+++ src/specs/internet.clj
@@ -1,7 +1,9 @@
 (ns specs.internet
-  (:require [clojure.spec :as s]
+  (:require [clojure.edn :as edn]
+            [clojure.spec :as s]
             [clojure.string :as string]
-            [clojure.test.check.generators :as gen]))
+            [clojure.test.check.generators :as gen])
+  (:import [java.util.regex Pattern]))
 
 (s/def ::hostpart
   (letfn [(pred [s]
@@ -29,12 +31,16 @@
   "Returns a spec for an Internet hostname that conforms to RFC1123. Options may
    be given via varargs:
 
-   :tlds - the set of allowed top-level domains
+   :domains - the set of allowed domains
    :min-depth - the minimum number of parts
    :max-depth - the maximum number of parts"
   [& options]
-  (let [{:keys [tlds min-depth max-depth]} options
-        hostpart-spec (s/get-spec ::hostpart)]
+  (let [{:keys [domains min-depth max-depth]} options
+        hostpart-spec (s/get-spec ::hostpart)
+        domain-re (when (seq domains)
+                    (re-pattern (str "\\."
+                                     (string/join "|" (map #(Pattern/quote (string/lower-case %)) domains))
+                                     "\\Z")))]
     (letfn [(pred [s]
               (and (>= 253 (count s))
                    (let [parts (string/split s #"\.")]
@@ -45,22 +51,24 @@
                           (not (string/starts-with? s "."))
                           (not (string/ends-with? s "."))
                           (every? (partial s/valid? hostpart-spec) parts)
-                          (or (not (seq tlds))
-                              (contains? tlds (last parts)))))))
+                          (or (not domain-re)
+                              (re-find domain-re (string/lower-case s)))))))
             (gen []
               (let [min-needed (or min-depth 2)
                     max-needed (or max-depth 4)
-                    tld-part (when (seq tlds)
-                               (gen/elements tlds))
-                    tld-count (if tld-part 1 0)
-                    parts-gen (gen/vector (s/gen hostpart-spec)
-                                          (- min-needed tld-count)
-                                          (- max-needed tld-count))]
-                (if tld-part
-                  (gen/let [tld tld-part
-                            parts parts-gen]
-                    (gen/return (string/join "." (conj parts tld))))
-                  (gen/fmap (partial string/join ".") parts-gen))))]
+                    domain-part (when (seq domains)
+                                  (gen/elements domains))]
+                (if domain-part
+                  (gen/let [domain domain-part]
+                    (let [domain-count (count (string/split domain #"\."))
+                          parts-gen (gen/vector (s/gen hostpart-spec)
+                                                (- min-needed domain-count)
+                                                (- max-needed domain-count))]
+                      (gen/let [parts parts-gen]
+                        (gen/return (string/join "." (conj parts domain))))))
+                  (let [parts-gen (gen/vector (s/gen hostpart-spec)
+                                              min-needed max-needed)]
+                    (gen/fmap (partial string/join ".") parts-gen)))))]
       (s/spec pred :gen gen))))
 
 (def tld-list
@@ -1442,10 +1450,10 @@
   (into #{} (map string/lower-case tld-list)))
 
 (s/def ::fully-qualified-hostname
-  (hostname :tlds tlds :min-depth 2))
+  (hostname :domains tlds :min-depth 2))
 
 (s/def ::fully-qualified-common-hostname
-  (hostname :tlds #{"com" "edu" "net" "org"} :min-depth 2 :max-depth 4))
+  (hostname :domains #{"com" "edu" "net" "org"} :min-depth 2 :max-depth 4))
 
 (s/def ::common-hostname
   (let [common-aliases #{"localhost"}]
@@ -1453,3 +1461,64 @@
      (s/or :alias common-aliases :hostname ::fully-qualified-common-hostname)
      :gen #(gen/one-of [(gen/elements common-aliases)
                         (s/gen (s/get-spec ::fully-qualified-common-hostname))]))))
+
+(s/def ::ip-address
+  (letfn [(pred [s]
+            (let [parts (string/split s #"\.")]
+              (and (= (count parts) 4)
+                   (every? (fn [part]
+                             (try
+                               (let [n (edn/read-string part)]
+                                 (and (integer? n)
+                                      (>= 256 n 0)))
+                               (catch Exception _ false)))
+                           parts))))
+          (gen []
+            (gen/fmap (partial string/join ".") (gen/vector (gen/choose 0 255) 4)))]
+    (s/spec pred :gen gen)))
+
+(s/def ::ip-port
+  (s/int-in 1 65536))
+
+(s/def ::privileged-ip-port
+  (s/int-in 1 1024))
+
+(s/def ::unprivileged-ip-port
+  (s/int-in 1025 65536))
+
+(s/def ::local-email-part
+  (let [chars (set (str "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                        "abcdefghijklmnopqrstuvwxyz"
+                        "0123456789"
+                        "!#$%&'*+-/=?^_`{|}~"))
+        dot-chars (conj chars \.)]
+    (letfn [(pred [s]
+              (and (>= 64 (count s) 1)
+                   (not (string/starts-with? s "."))
+                   (not (string/ends-with? s "."))
+                   (every? dot-chars s)
+                   (not (re-find #"\.\." s))))
+            (gen []
+              (gen/fmap string/join (gen/vector (gen/elements chars) 1 64)))]
+      (s/spec pred :gen gen))))
+
+(defn email-address
+  [& options]
+  (let [{:keys [hosts domains]} options
+        hostname-spec (hostname :domains domains)
+        local-spec (s/get-spec ::local-email-part)
+        hosts (when (seq hosts) (set (map string/lower-case hosts)))]
+    (letfn [(pred [s]
+              (let [parts (string/split s #"@")]
+                (and (= 2 (count parts))
+                     (s/valid? local-spec (first parts))
+                     (if (seq hosts)
+                       (contains? hosts (string/lower-case (second parts)))
+                       (s/valid? hostname-spec (second parts))))))
+            (gen []
+              (gen/let [local-part (s/gen local-spec)
+                        hostname-part (if (seq hosts)
+                                        (gen/elements hosts)
+                                        (s/gen hostname-spec))]
+                (gen/return (str local-part "@" hostname-part))))]
+      (s/spec pred :gen gen))))
