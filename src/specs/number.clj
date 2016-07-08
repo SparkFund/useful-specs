947213879f40e49460e370ce020799103e174dc0
Donald Ball <donald.ball@gmail.com>
2016-07-08 14:44:12 -0400

    Use the precision in the generator if given
    Specifically, the precision implies a min and a max value when not
    given, and generated doubles must be rounded to the nearest precision

--- src/specs/number.clj
+++ src/specs/number.clj
@@ -1,7 +1,7 @@
 (ns specs.number
   (:require [clojure.spec :as s]
             [clojure.test.check.generators :as gen])
-  (:import [java.math BigDecimal]))
+  (:import [java.math BigDecimal MathContext RoundingMode]))
 
 (defn decimal-in
   "Specs a decimal number. The number type may be anything that bigdec
@@ -10,7 +10,11 @@
     :precision - the number of digits in the unscaled value (default none)
     :scale     - the number of digits to the right of the decimal (default none)
     :min       - minimum value (inclusive, default none)
-    :max       - maximum value (inclusive, default none)"
+    :max       - maximum value (inclusive, default none)
+
+   Note that the java math definition of precision and scale may not be the
+   same as e.g. your database. For example, -1E-75M has a precision of 1 and a
+   scale of 75. For sanest results, you should specify both."
   [& options]
   (let [{:keys [precision scale min max]} options]
     (when (and min max)
@@ -36,10 +40,25 @@
                            (>= max d))))
                 (catch Exception _ false)))
             (gen []
-              (gen/let [p (gen/double* {:infinite? false :NaN? false :min min :max max})]
-                (cond-> (bigdec p)
-                  scale
-                  (.setScale scale BigDecimal/ROUND_HALF_UP))))]
+              (let [min (or min
+                            (and precision
+                                 (-> (BigDecimal/ONE)
+                                     (.movePointRight precision)
+                                     dec
+                                     .negate)))
+                    max (or max
+                            (and precision
+                                 (-> (BigDecimal/ONE)
+                                     (.movePointRight precision)
+                                     dec)))]
+                (gen/let [p (gen/double* {:infinite? false :NaN? false :min min :max max})]
+                  (let [mc (when precision
+                             (MathContext. precision RoundingMode/HALF_UP))]
+                    (cond-> (bigdec p)
+                      scale
+                      (.setScale scale BigDecimal/ROUND_HALF_UP)
+                      precision
+                      (.round mc))))))]
       (when min
         (assert (pred min)))
       (when max
